4904a4736300fbd287cd6cf107a9c653da750781
Rafael Sobek <rafael.sobek@googlemail.com>
2011-01-05 10:36:47 +0100

    add

---
+++ osgi-blackbox-archetype/src/main/java/org/developers/blog/osgi/blackbox/api/IBlackBoxService.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.developers.blog.osgi.blackbox.api;
+
+/**
+ *
+ * @author rafsob
+ */
+public interface IBlackBoxService {
+
+}
